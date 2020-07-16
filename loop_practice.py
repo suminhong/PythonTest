5fcc0b5268bd165ecd04b2c62818b0e3e89396e6
suminhog <sci97@naver.com>
2020-07-16 15:49:49 +0900

    소수

--- loop_practice.py
+++ loop_practice.py
@@ -24,14 +24,16 @@ while True :
 
 #4. 1부터 100 사이의 소수 출력하고 개수 구하라
 count = 0
+print("1부터 100까지의 소수는")
 for x in range(1, 100) :
     x += 1
-    #print("\nx는", x)
+    #print("\nx : ", x)
     for i in range(int(x/2), 0, -1) :
         #print(i, end=' ')
-        if i <= 2 :
+        if i < 2 :
             print(x, end=', ')
             count += 1
+            break
         if x % i == 0 :
             break
 
